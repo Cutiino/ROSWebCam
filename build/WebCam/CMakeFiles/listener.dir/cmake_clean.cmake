677b6e18b84fa9ce700463830464d9e1ec6ba06f
Agustin <agustin.cutino@gmail.com>
2021-07-19 22:53:42 -0400

    Se agrega paquete para subscribirse a topic de imagen. Problema con openCV y para ejecutar el nodo suscriptor

---
+++ build/WebCam/CMakeFiles/listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/agustin/TareaRos/devel/lib/usb_cam/listener"
+  "/home/agustin/TareaRos/devel/lib/usb_cam/listener.pdb"
+  "CMakeFiles/listener.dir/nodes/listener.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
